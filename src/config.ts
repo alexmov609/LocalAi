13920851ac05174280860c1043622d404240675a
alexmovchan100 <alex.m@esn.co.il>
2026-04-19 19:35:48 +0300

    Add env.example. Add config constanst

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 //constants, types, and other configuration values for the project
 export const CONFIG = {
     model: 'qwen3:8b',
-    // понадобится на шагах 5-6
     dataDir: './data',
 } as const;
